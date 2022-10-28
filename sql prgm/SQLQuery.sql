0e575bb6838ec8150465a8230034e54bb29b9950
KhadeejaShirinVP <khadeejasvp@moback.com>
2022-10-28 21:21:32 +0530

    Update notes and SQL Queries

--- sql prgm/SQLQuery.sql
+++ sql prgm/SQLQuery.sql
@@ -7,8 +7,8 @@ create table Employee
 EmpID INT not null primary key,
 EmpName varchar(100),
 EmpAge int,
-SalaryID int not null,
-DepID int NOT NULL
+SalaryID int not null FOREIGN KEY REFERENCES [dbo].[Salary](SalaryID),
+DepID int NOT NULL FOREIGN KEY REFERENCES [dbo].[Department](DepID)
 );
 
 
@@ -16,7 +16,7 @@ DepID int NOT NULL
 --insert values
 insert into Employee(EmpID,EmpName,EmpAge,SalaryID,DepID) values (101,'Pavan',34,211,1);
 insert into Employee(EmpID,EmpName,EmpAge,SalaryID,DepID) values (102,'Akhil',33,212,2);
-insert into Employee(EmpID,EmpName,EmpAge,SalaryID,DepID) values (103,'Fidha',24,213,3);
+insert into Employee(EmpID,EmpName,EmpAge,SalaryID,DepID) values (103,'Fidha',24,213,2);
 insert into Employee(EmpID,EmpName,EmpAge,SalaryID,DepID) values (104,'Kavya',28,214,4);
 
 SELECT * FROM Employee
@@ -54,6 +54,8 @@ select * from Salary
 select * from Department
 SELECT * FROM Employee
 
+
+
 --inner join
 select Employee.EmpName,Department.DepName,Salary.SalaryAmt
 from((Employee
@@ -61,3 +63,85 @@ inner join Department on Employee.DepID=Department.DepID)
 inner join Salary on Employee.SalaryID=Salary.SalaryID);
 
 ----------------------------------------------------------------------------------------
+--create stored procedure FOR INSERT
+create proc InsertData(
+						@EmpID INT,
+						@EmpName VARCHAR(100),
+						@EmpAge INT,
+						@SalaryID INT, 
+						@DepID INT
+						)
+AS
+BEGIN
+			INSERT INTO Employee
+					(EmpID,EmpName,EmpAge,SalaryID,DepID)
+			VALUES
+					(@EmpID,@EmpName,@EmpAge,@SalaryID,@DepID)
+END
+
+exec InsertData 107,'Kallyan',29,211,3
+------DISPLAY THE TABLE
+--create view GetEmplyeeDetails
+--select EmpName,EmpAge
+--from Employee
+--where 
+	...............................................................................................................
+	CREATE VIEW GetEmplyeeDetails
+	as
+	SELECT EmpName,DepName,SalaryAmt
+	from Employee inner join Department on Employee.DepID=Department.DepID
+	inner join Salary on Employee.SalaryID=Salary.SalaryID
+
+	select * from GetEmplyeeDetails
+....................................................................................................................
+--update
+create proc UpdateEmployee
+@EmpID int,
+@EmpName varchar (100)
+as
+begin
+	update Employee set EmpName=@EmpName where EmpID=@EmpID;
+END
+
+EXEC UpdateEmployee 102,'ANSHI'
+
+................................................................................................................
+--DELETE
+create proc DeleteDetails
+@EmpID int
+as
+begin
+	delete from Employee where EmpID=@EmpID
+end
+
+--exec DeleteDetails 103
+--------------------------------------------------------------------------------------------------------------------------------------
+
+--dePARTMENT SP CREATION
+CREATE PROC InsertDep
+					(@DepID int,
+					 @DepName varchar(100) 
+					)
+as
+begin
+			insert into Department
+					(DepID,DepName)
+			values 
+					(@DepID,@DepName)
+end
+
+exec InsertDep 5,'database'
+
+---------------------------------------------------------------------------------------------------------------------------------------
+--update the department
+create proc UpdateDepartment
+@DepID int,
+@DepName varchar (100)
+as
+begin
+	update Department set DepName=@DepName where DepID=@DepID;
+END
+
+EXEC UpdateDepartment 4,'QA'
+
+-----------------------------------------------------------------------------------------------------------------------------
